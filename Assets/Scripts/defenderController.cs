c563a03e2c067cd4bee6ba992d2c66731c3b6f1b
andrewstyll <andrewstyll@gmail.com>
2017-04-04 03:34:17 -0400

    added base assets

---
+++ Assets/Scripts/defenderController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class defenderController : MonoBehaviour {
+
+	//define camera to control bounds of window
+	public Camera camera;
+
+	// Use this for initialization
+	void Start () {
+		if (camera == null) {
+			camera = Camera.main;
+		}
+	}
+	
+	// called once per physics step
+	void FixedUpdate () {
+	
+	}
+}
---
+++ Assets/Scripts/invaderBehaviour.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class invaderBehaviour : MonoBehaviour {
+
+	//store a reference to the two sprites to allow switching between the two
+	public Sprite position_1;
+	public Sprite position_2;
+
+	private SpriteRenderer spriteRenderer;
+
+	// Use this for initialization
+	void Start () {
+		spriteRenderer = GetComponent<SpriteRenderer> ();
+		if(spriteRenderer.sprite == null) {
+			spriteRenderer.sprite = position_1;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyUp (KeyCode.Space)) {
+			changeSprite ();
+		}
+	}
+
+	void changeSprite() {
+		if (spriteRenderer.sprite == position_1) {
+			spriteRenderer.sprite = position_2;
+		} else {
+			spriteRenderer.sprite = position_1;	
+		}
+	}
+}
