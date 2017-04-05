f0c16f8666fd137d5faaa8e868d37b4b04a7a25b
andrewstyll <andrewstyll@gmail.com>
2017-04-05 03:16:26 -0400

    added player movement and game controller

--- Assets/Scripts/defenderController.cs
+++ Assets/Scripts/defenderController.cs
@@ -4,17 +4,43 @@ using System.Collections;
 public class defenderController : MonoBehaviour {
 
 	//define camera to control bounds of window
-	public Camera camera;
+	public Camera cam;
+	public float maxSpeed;
+
+	private Rigidbody2D defender;
+	private float maxWidth;
+	private float defenderWidth;
 
 	// Use this for initialization
 	void Start () {
-		if (camera == null) {
-			camera = Camera.main;
+		if (cam == null) {
+			cam = Camera.main;
 		}
+		if (maxSpeed == 0.0f) {
+			maxSpeed = 5f;
+		}
+
+		defender = GetComponent<Rigidbody2D> ();
+
+		defenderWidth = GetComponent<Renderer> ().bounds.extents.x;
+		Vector3 maxCorner = new Vector3 (Screen.width, Screen.height, 0.0f);
+		Vector3 maxCornerW = cam.ScreenToWorldPoint(maxCorner);
+		maxWidth = maxCornerW.x - defenderWidth/2;
 	}
 	
-	// called once per physics step
+	// called once per physics timestep
 	void FixedUpdate () {
-	
+	//need to find pointer first then use that to move defender
+
+		Vector3 pointPosition = cam.ScreenToWorldPoint(Input.mousePosition);
+
+		float targetPositionWidth = Mathf.Clamp (pointPosition.x, -maxWidth, maxWidth);
+		Vector3 targetPosition = new Vector3 (targetPositionWidth, defender.position.y, 0.0f);
+
+		Vector3 currentPosition = new Vector3 (defender.position.x, defender.position.y, 0.0f);
+
+		//don't normalise, we want to defender to coast in to the destination position
+		Vector3 direction = (targetPosition - currentPosition);
+		defender.MovePosition (currentPosition + direction * maxSpeed * Time.deltaTime);
 	}
 }
---
+++ Assets/Scripts/gameController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// the purpose of this script is to primarily keep track fo all instances of objects 
+// in the game :D
+
+public class gameController : MonoBehaviour {
+
+	public GameObject invader_1;
+	public GameObject invader_2;
+	public GameObject invader_3;
+
+	//linked list would be better
+	private int MAX_ROWS = 1;
+	private int MAX_COLUMNS = 11;
+	private Object[,] invaders;
+
+	// Use this for initialization
+	void Start () {
+		invaders = new Object[MAX_ROWS, MAX_COLUMNS];
+
+		for (int i = 0; i < MAX_ROWS; i++) {
+			for (int j = 0; i < MAX_COLUMNS; j++) {
+				Vector3 position = new Vector3 (0.0f, 0.0f, 0.0f);
+				Quaternion rotation = Quaternion.identity;
+				invaders[i, j] = Instantiate (invader_1, position, rotation);
+			}
+		}
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
